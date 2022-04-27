3ce15e66e81093f827e1cb4596223856d9b6bd00
Michaela Boctario <mbucatariu24@gmail.com>
2022-04-27 11:41:22 +0200

    add validation on register page

--- groupomania-vue/src/pages/RegisterUser.vue
+++ groupomania-vue/src/pages/RegisterUser.vue
@@ -4,49 +4,101 @@
 
       <div class="flex-container justify-center">
           <div class="col-7">
-              <form @submit.prevent="register" class="card card-form">
+              <!-- print form values -->
+              <!-- <Form @submit="register" :validation-schema="schema" v-slot="{ values }">               
+                  <pre>{{ values }}</pre> -->
+              <Form @submit="register" :validation-schema="schema">               
                   <h2 class="text-center">Register</h2>
-
+                  
                   <div class="input-group">
                       <label for="firstname">Prénom</label>
-                      <input v-model="form.firstname" id="firstname" type="text" class="form-input">
+                      <Field v-model="form.firstname" id="firstname" name="firstname" type="text" class="form-input" />
+                      <ErrorMessage name="firstname" class="form-error" />
                   </div>
 
                   <div class="input-group">
                       <label for="lastname">Nom</label>
-                      <input v-model="form.lastname" id="lastname" type="text" class="form-input">
+                      <Field v-model="form.lastname" id="lastname" name="lastname" type="text" class="form-input" />
+                      <ErrorMessage name="lastname" class="form-error" />
                   </div>
 
                   <div class="input-group">
                       <label for="username">Nom d'utilisateur</label>
-                      <input v-model="form.username" id="username" type="text" class="form-input">
+                      <Field v-model="form.username" id="username" name="username" type="text" class="form-input" />
+                      <ErrorMessage name="username" class="form-error" />
                   </div>
 
                   <div class="input-group">
                       <label for="email">Email</label>
-                      <input v-model="form.email" id="email" type="email" class="form-input">
+                      <Field v-model="form.email" id="email" name="email" type="email" class="form-input" />
+                      <ErrorMessage name="email" class="form-error" />
                   </div>
 
                   <div class="input-group">
                       <label for="password">Mot de passe</label>
-                      <input v-model="form.password" id="password" type="password" class="form-input">
+                      <Field v-model="form.password" id="password" name="password" type="password" class="form-input" />
+                      <ErrorMessage name="password" class="form-error" />
                   </div>
-
+                  
                   <div class="form-actions">
                       <button type="submit" class="btn-blue btn-block">Register</button>
                   </div>
 
-              </form>
+                  <div
+                    v-if="message"
+                    :class="successful ? 'alert-success' : 'alert-error'"
+                  >
+                    {{ message }}
+                  </div>  
+              </Form>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
 
 export default {
+  name: "RegisterUser",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data () {
+    const schema = yup.object().shape({
+      username: yup
+        .string()
+        .required('Le nom d\'utilisateur est obligatoire!')
+        .min(3, 'Il doit contenir minimum 3 caractères!')
+        .max(20, 'Il doit contenir maximum 20 caractères!'),
+      email: yup
+        .string()
+        .required('L\'addresse email est obligatoire !')
+        .email('L\'addresse email est invalide!')
+        .max(50, 'Elle doit contenir maximum 50 caractères!'),
+      password: yup
+        .string()
+        .required('Le mot de passe est obligatoire!')
+        .min(8, 'Il doit contenir minimum 8 caractères!')
+        .max(40, 'Il doit contenir maximum 40 caractères!'),
+      firstname: yup
+        .string()
+        .required('Le nom de l\'utilisateur est obligatoire!')
+        .min(3, 'Il doit contenir minimum 3 caractères!')
+        .max(40, 'Il doit contenir maximum 40 caractères!'),
+      lastname: yup
+        .string()
+        .required('Le prénom de l\'utilisateur est obligatoire!')
+        .min(3, 'Il doit contenir minimum 3 caractères!')
+        .max(40, 'Il doit contenir maximum 40 caractères!'),  
+    });
     return {
+      successful: false,
+      message: '',  
+      schema,
       form: {
         firstname: '',
         lastname: '',
@@ -58,18 +110,15 @@ export default {
   },
   methods: {
     register() {
-      console.log("register")
-      console.log(this.form)
+      // console.log("register")
+      // console.log(this.form)
       this.message = "";
       this.successful = false;
-      this.loading = true;
-
       this.$store.dispatch("auth/register", this.form).then(
         data => {
-          console.log(data)
+          //console.log(data)
           this.message = data.message;
           this.successful = true;
-          this.loading = false;
            //this.$router.push('/')
         },
         error => {
@@ -80,7 +129,7 @@ export default {
             error.message ||
             error.toString();
           this.successful = false;
-          this.loading = false;
+          //console.log(this.message);
         }
       );
     },
